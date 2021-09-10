8b6313103d67986343d58fae540a3e3e77b76f17
Axel Garrido <axel_g10@hotmail.com>
2021-09-10 06:45:49 -0300

    add: proyecto mercadoliebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./view/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./view/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./view/login.html"));
+});
+
+const puerto = process.env.PORT || 3000
+
+app.listen(puerto, () => {
+  console.log(`Server is running on PORT : ${puerto}`);
+});
